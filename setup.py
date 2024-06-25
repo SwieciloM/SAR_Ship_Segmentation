30be0305364bdbd25acdee32205f991ece0c6a69
Michał Święciło <michal_00-00@o2.pl>
2024-06-25 22:22:03 +0200

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning project focused on ship segmentation in high-resolution SAR images.',
+    author='Michal Swiecilo',
+    license='MIT',
+)
